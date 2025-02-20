5fd2f35d0a842b11220cd7bcd73034c2499caf3e
ankuxsshh <ankuxshh72@gmail.com>
2025-02-20 13:24:20 +0530

    first commit

--- karunya_app/urls.py
+++ karunya_app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
+    path('appoinment', views.appoinment, name='appoinment'),    
+    path('department', views.department, name='department'),   
+    path('doctor', views.doctor, name='doctor'),   
 ]
--- karunya_app/views.py
+++ karunya_app/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def department(request):
+    return render(request, 'department.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def appoinment(request):
+    return render(request, 'appoinment.html')
+
+def doctor(request):
+    return render(request, 'doctor.html')
