570d3593f8e8c18c913784937a6b2c967eda7c1e
ankuxsshh <ankuxshh72@gmail.com>
2025-06-11 04:10:01 +0530

    firstcommit

--- karunya_app/admin.py
+++ karunya_app/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Testimonial, InsuranceCompany
 from django.contrib.auth.models import User, Group
     
-admin.site.register(Testimonial)
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'created_at']
+    
 admin.site.register(InsuranceCompany)
 
 # Unregister User and Group from the admin panel
--- karunya_app/models.py
+++ karunya_app/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class Testimonial(models.Model):
     name = models.CharField(max_length=100, default='')
-    title = models.CharField(max_length=150)
     message = models.TextField()
     image = models.ImageField(upload_to='testimonials/')
+    rating = models.IntegerField(default=5)  # Rating out of 5
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} - {self.title}"
+        return f"{self.name} - {self.rating} Stars"
 
 
 class InsuranceCompany(models.Model):
