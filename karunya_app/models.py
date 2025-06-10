b52da263861a5e5aa55f8a6697dc79306c17736a
ankuxsshh <ankuxshh72@gmail.com>
2025-06-10 16:07:03 +0530

    firstcommit

--- karunya_app/models.py
+++ karunya_app/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 
 class Testimonial(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default='')
     title = models.CharField(max_length=150)
     message = models.TextField()
     image = models.ImageField(upload_to='testimonials/')
